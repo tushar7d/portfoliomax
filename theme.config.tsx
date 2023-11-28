c31961b95731741daa8318afdfd746d70b4c944e
Tushar Debnath <41472705+tushar7d@users.noreply.github.com>
2023-11-28 23:51:38 +0530

    changes

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,14 +2,9 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
-  },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  logo: <span>Tushar Debnath</span>,
+  search:{component: false},
+  
   footer: {
     text: 'Nextra Docs Template',
   },
