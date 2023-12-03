0a6286edf57c00f9043f2b1e8fea6e487d78edf3
Tushar Debnath <41472705+tushar7d@users.noreply.github.com>
2023-12-03 21:17:16 +0530

    changes

--- theme.config.jsx
+++ theme.config.jsx
@@ -7,7 +7,16 @@ const config = {
     return (
       <>
        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Tushar Debnath</title>
+       <title>Tushar Debnath</title>
+        <meta property="og:title" content="Tushar Debnath" />
+        <meta property="og:site_name" content="Tushar Debnath" />
+        <meta property="og:url" content="https://tushardebnath.com" />
+        <meta property="og:image" content="https://tushardebnath.com/og.png" />
+        <meta
+          property="og:description"
+          content="Product designer and developer from New Delhi"
+        />
+        <meta property="og:type" content="website" />
       </>
     );
   },
